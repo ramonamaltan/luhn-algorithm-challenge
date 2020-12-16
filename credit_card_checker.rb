d5bdd2bead3fd364602286d4ff8dbdd1523920e0
Ramona Maltan <ramonamaltan@gmail.com>
2020-12-16 16:37:59 +0100

    refactoring for odd and even indexes and added comments

--- credit_card_checker.rb
+++ credit_card_checker.rb
@@ -1,28 +1,26 @@
 def valid_card?(card)
-  # If the result of this doubling operation is greater than 9 (e.g., 8 * 2 = 16),
-  # add the digits of the product (e.g., 16: 1 + 6 = 7, 18: 1 + 8 = 9)
+  # whitespaces shouldn't matter
   array = card.gsub(' ', '').split('')
-  index = [-2, -4, -6, -8, -10, -12, -14, -16]
   sum = 0
-  index.each do |i|
-    if array[i].to_i * 2 < 9
-      sum += array[i].to_i * 2
-    else
-      digits = (array[i].to_i * 2).to_s.split('')
-      digits.each do |digit|
-        sum += digit.to_i
+  # Starting from the rightmost digit double the value of every second digit --> index even
+  array.each_with_index do |element, index|
+    if index.even?
+      if element.to_i * 2 < 9
+        sum += element.to_i * 2
+      else
+        # If the result of this doubling operation is greater than 9 (e.g. 8 * 2 = 16),
+        # add the digits of the product (e.g. 16: 1 + 6 = 7)
+        digits = (element.to_i * 2).to_s.split('')
+        digits.each do |digit|
+          sum += digit.to_i
+        end
       end
+    else
+      # write down the rest of the numbers directly & sum up
+      sum += element.to_i
     end
   end
-
-  # write down the rest of the numbers directly & sum up
-  index_odd = [-1, -3, -5, -7, -9, -11, -13, -15]
-  index_odd.each do |i|
-    sum += array[i].to_i
-  end
-
   # if modulo 10 equal to 0 true else false
-
   (sum % 10).zero? && card != ''
 end
 
